7923ba086fff71c9d0821a54526cbe31f914fcbe
argaldo <argaldo@gmail.com>
2011-02-03 11:19:03 +0100

    iPhone configuration view

---
+++ iPhone/Views/IPhoneSimulationConfigurationView.h
@@ -0,0 +1,16 @@
+//
+//  IPhoneSimulationConfigurationView.h
+//  iAGC
+//
+//  Created by argaldo on 2/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface IPhoneSimulationConfigurationView : UIViewController {
+
+}
+
+@end
