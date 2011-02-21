1a20d588d2383b4fec505d0178539746ad6ea929
argaldo <argaldo@gmail.com>
2011-02-21 17:13:49 +0100

    Shared DSKY View

---
+++ Shared/Views/SharedDSKYViewController.h
@@ -0,0 +1,120 @@
+//
+//  SharedDSKYViewController.h
+//  iAGC
+//
+//  Created by argaldo on 2/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UpdateDSKYUserInterfaceDelegate.h"
+#import "DSKYSimulationClient.h"
+
+
+@interface SharedDSKYViewController : UIViewController <UpdateDSKYUserInterfaceDelegate> {
+	DSKYSimulationClient *dskySimulationClient;
+@private
+	NSMutableDictionary *segments;
+	
+	IBOutlet UIImageView *M1Outlet;
+	IBOutlet UIImageView *M2Outlet;
+	
+	IBOutlet UIImageView *V1Outlet;
+	IBOutlet UIImageView *V2Outlet;
+	
+	IBOutlet UIImageView *N1Outlet;
+	IBOutlet UIImageView *N2Outlet;
+	
+	IBOutlet UIImageView *CompActIndOutlet;
+	
+	IBOutlet UIImageView *uplinkActivity;
+	IBOutlet UIImageView *noAttitude;
+	IBOutlet UIImageView *standBy;
+	IBOutlet UIImageView *keyRelease;
+	IBOutlet UIImageView *operationError;
+	IBOutlet UIImageView *priorityDisplay;
+	IBOutlet UIImageView *noDAP;
+	IBOutlet UIImageView *temp;
+	IBOutlet UIImageView *gimbalLock;
+	IBOutlet UIImageView *prog;
+	IBOutlet UIImageView *restart;
+	IBOutlet UIImageView *tracker;
+	IBOutlet UIImageView *alt;
+	IBOutlet UIImageView *vel;
+	
+	
+	IBOutlet UIImageView *_r1plusminus;
+	IBOutlet UIImageView *_11Outlet;	
+	IBOutlet UIImageView *_12Outlet;
+	IBOutlet UIImageView *_13Outlet;
+	IBOutlet UIImageView *_14Outlet;
+	IBOutlet UIImageView *_15Outlet;
+	
+	IBOutlet UIImageView *_r2plusminus;
+	IBOutlet UIImageView *_21Outlet;	
+	IBOutlet UIImageView *_22Outlet;
+	IBOutlet UIImageView *_23Outlet;
+	IBOutlet UIImageView *_24Outlet;
+	IBOutlet UIImageView *_25Outlet;
+	
+	IBOutlet UIImageView *_r3plusminus;
+	IBOutlet UIImageView *_31Outlet;	
+	IBOutlet UIImageView *_32Outlet;
+	IBOutlet UIImageView *_33Outlet;
+	IBOutlet UIImageView *_34Outlet;
+	IBOutlet UIImageView *_35Outlet;
+}
+
+@property (nonatomic,retain) DSKYSimulationClient *dskySimulationClient;
+
+@property (nonatomic,retain) IBOutlet UIImageView *M1Outlet;
+@property (nonatomic,retain) IBOutlet UIImageView *M2Outlet;
+
+@property (nonatomic,retain) IBOutlet UIImageView *V1Outlet;
+@property (nonatomic,retain) IBOutlet UIImageView *V2Outlet;
+
+@property (nonatomic,retain) IBOutlet UIImageView *N1Outlet;
+@property (nonatomic,retain) IBOutlet UIImageView *N2Outlet;
+
+@property (nonatomic,retain) IBOutlet UIImageView *CompActIndOutlet;
+
+@property (nonatomic,retain) IBOutlet UIImageView *uplinkActivity;
+@property (nonatomic,retain) IBOutlet UIImageView *noAttitude;
+@property (nonatomic,retain) IBOutlet UIImageView *standBy;
+@property (nonatomic,retain) IBOutlet UIImageView *keyRelease;
+@property (nonatomic,retain) IBOutlet UIImageView *operationError;
+@property (nonatomic,retain) IBOutlet UIImageView *priorityDisplay;
+@property (nonatomic,retain) IBOutlet UIImageView *noDAP;
+@property (nonatomic,retain) IBOutlet UIImageView *temp;
+@property (nonatomic,retain) IBOutlet UIImageView *gimbalLock;
+@property (nonatomic,retain) IBOutlet UIImageView *prog;
+@property (nonatomic,retain) IBOutlet UIImageView *restart;
+@property (nonatomic,retain) IBOutlet UIImageView *tracker;
+@property (nonatomic,retain) IBOutlet UIImageView *alt;
+@property (nonatomic,retain) IBOutlet UIImageView *vel;
+
+
+@property (nonatomic,retain) IBOutlet UIImageView *_r1plusminus;
+@property (nonatomic,retain) IBOutlet UIImageView *_11Outlet;	
+@property (nonatomic,retain) IBOutlet UIImageView *_12Outlet;
+@property (nonatomic,retain) IBOutlet UIImageView *_13Outlet;
+@property (nonatomic,retain) IBOutlet UIImageView *_14Outlet;
+@property (nonatomic,retain) IBOutlet UIImageView *_15Outlet;
+
+@property (nonatomic,retain) IBOutlet UIImageView *_r2plusminus;
+@property (nonatomic,retain) IBOutlet UIImageView *_21Outlet;	
+@property (nonatomic,retain) IBOutlet UIImageView *_22Outlet;
+@property (nonatomic,retain) IBOutlet UIImageView *_23Outlet;
+@property (nonatomic,retain) IBOutlet UIImageView *_24Outlet;
+@property (nonatomic,retain) IBOutlet UIImageView *_25Outlet;
+
+@property (nonatomic,retain) IBOutlet UIImageView *_r3plusminus;
+@property (nonatomic,retain) IBOutlet UIImageView *_31Outlet;	
+@property (nonatomic,retain) IBOutlet UIImageView *_32Outlet;
+@property (nonatomic,retain) IBOutlet UIImageView *_33Outlet;
+@property (nonatomic,retain) IBOutlet UIImageView *_34Outlet;
+@property (nonatomic,retain) IBOutlet UIImageView *_35Outlet;
+
+@property (nonatomic,retain) NSMutableDictionary *segments;
+
+@end
---
+++ Shared/Views/SharedDSKYViewController.m
@@ -0,0 +1,212 @@
+    //
+//  SharedDSKYViewController.m
+//  iAGC
+//
+//  Created by argaldo on 2/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SharedDSKYViewController.h"
+#import "DSKYUIIPadViewController.h"
+#import "Util.h"
+
+@implementation SharedDSKYViewController
+
+@synthesize segments;
+@synthesize dskySimulationClient;
+@synthesize M1Outlet,M2Outlet,V1Outlet,V2Outlet,N1Outlet,N2Outlet,CompActIndOutlet,uplinkActivity,noAttitude,standBy,keyRelease,operationError,priorityDisplay,noDAP,temp,gimbalLock,prog,restart,tracker,alt,vel,_r1plusminus,_11Outlet,	_12Outlet,_13Outlet,_14Outlet,_15Outlet,_r2plusminus,_21Outlet,_22Outlet,_23Outlet,_24Outlet,_25Outlet,_r3plusminus,_31Outlet,_32Outlet,_33Outlet,_34Outlet,_35Outlet;
+
+
+// The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+/*
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization.
+    }
+    return self;
+}
+*/
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView {
+}
+*/
+
+
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations.
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc. that aren't in use.
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+- (void) initSegmentDictionary {	
+	//TODO change this to use a NSDictionary ... 
+	// initializing segment dictionary
+	segments = [[NSMutableDictionary alloc] init];
+	[segments setObject:self.M1Outlet forKey:@"M1"];
+	[segments setObject:self.M2Outlet forKey:@"M2"];
+	[segments setObject:self.V1Outlet forKey:@"V1"];
+	[segments setObject:self.V2Outlet forKey:@"V2"];
+	[segments setObject:self.N1Outlet forKey:@"N1"];
+	[segments setObject:self.N2Outlet forKey:@"N2"];
+	[segments setObject:self._11Outlet forKey:@"11"];
+	[segments setObject:self._12Outlet forKey:@"12"];
+	[segments setObject:self._13Outlet forKey:@"13"];
+	[segments setObject:self._14Outlet forKey:@"14"];
+	[segments setObject:self._15Outlet forKey:@"15"];
+	[segments setObject:self._21Outlet forKey:@"21"];
+	[segments setObject:self._22Outlet forKey:@"22"];
+	[segments setObject:self._23Outlet forKey:@"23"];
+	[segments setObject:self._24Outlet forKey:@"24"];
+	[segments setObject:self._25Outlet forKey:@"25"];
+	[segments setObject:self._31Outlet forKey:@"31"];
+	[segments setObject:self._32Outlet forKey:@"32"];
+	[segments setObject:self._33Outlet forKey:@"33"];
+	[segments setObject:self._34Outlet forKey:@"34"];
+	[segments setObject:self._35Outlet forKey:@"35"];
+	[segments setObject:self._r1plusminus forKey:@"R1"];
+	[segments setObject:self._r2plusminus forKey:@"R2"];
+	[segments setObject:self._r3plusminus forKey:@"R3"];
+	[segments setObject:self.CompActIndOutlet forKey:@"CompActInd"];
+	[segments setObject:self.operationError forKey:@"opError"];
+	[segments setObject:self.keyRelease forKey:@"keyRel"];
+	[segments setObject:self.uplinkActivity forKey:@"uplinkActivity"];
+	[segments setObject:self.temp forKey:@"tempCaution"];
+	[segments setObject:self.vel forKey:@"VelInd"];
+	[segments setObject:self.noAttitude forKey:@"NoAttInd"];
+	[segments setObject:self.alt forKey:@"AltInd"];
+	[segments setObject:self.gimbalLock forKey:@"GimbalLockInd"];
+	[segments setObject:self.tracker forKey:@"TrackerInd"];
+	[segments setObject:self.prog forKey:@"ProgInd"];
+}
+
+- (void) sendDSKYCode:(int) code {
+	// wrapper over DSKYSimulationClient instance to send dsky data
+	[dskySimulationClient sendDSKYCode:code];
+}
+
+
+// key handling code
+- (IBAction) pressedVerb: (id) sender{
+	[self sendDSKYCode:17];
+}
+
+- (IBAction) pressedNoun: (id) sender{
+	[self sendDSKYCode:31];
+}
+
+- (IBAction) pressed1: (id) sender{
+	[self sendDSKYCode:1];
+}
+- (IBAction) pressed2: (id) sender{
+	[self sendDSKYCode:2];
+}
+
+- (IBAction) pressed3: (id) sender{
+	[self sendDSKYCode:3];
+}
+- (IBAction) pressed4: (id) sender{
+	[self sendDSKYCode:4];
+}
+- (IBAction) pressed5: (id) sender{
+	[self sendDSKYCode:5];
+}
+- (IBAction) pressed6: (id) sender{
+	[self sendDSKYCode:6];
+}
+- (IBAction) pressed7: (id) sender{
+	[self sendDSKYCode:7];
+}
+- (IBAction) pressed8: (id) sender{
+	[self sendDSKYCode:8];
+}
+- (IBAction) pressed9: (id) sender{
+	[self sendDSKYCode:9];
+}
+- (IBAction) pressed0: (id) sender{
+	[self sendDSKYCode:16];
+}
+- (IBAction) pressedEnter: (id) sender{
+	[self sendDSKYCode:28];
+}
+
+- (IBAction) pressedClr: (id) sender {
+	[self sendDSKYCode:30];
+}
+- (IBAction) pressedPro: (id) sender {
+	//falta este
+	[self sendDSKYCode:28];
+}
+- (IBAction) pressedKeyRel: (id) sender{
+	[self sendDSKYCode:25];
+}
+- (IBAction) pressedPlus: (id) sender{
+	[self sendDSKYCode:26];
+}
+
+- (void) changeSegmentValue: (NSMutableArray *) args {
+	NSString *component = [args objectAtIndex:0];
+	NSString *imageName = (NSString *)[args objectAtIndex:1];
+	
+	if ([imageName compare:@""] != NSOrderedSame) {
+		UIImageView *segmentImage = [self.segments objectForKey:component];
+		[segmentImage setImage:[UIImage imageNamed:imageName]];
+		segmentImage.animationImages = [NSArray arrayWithObjects:[UIImage imageNamed:imageName],[UIImage imageNamed:@"digit-off.jpg"],nil];
+	}
+}
+
+- (void) updateUserInterface:(NSString **)component withValue:(int) value withComponentType:(int) componentType {
+	[self updateUserInterface:component withValue:value withComponentType:componentType withComponentSubtype:0];
+}
+- (void) updateUserInterface:(NSString **)component withValue:(int) value withComponentType:(int) componentType withComponentSubtype:(int) componentSubtype {
+	switch (componentType){
+		case SEGMENT:
+			[self performSelectorOnMainThread:@selector(changeSegmentValue:) withObject:[NSMutableArray arrayWithObjects:*component,[Util getImageNameForSegmentValue:value],nil] waitUntilDone:YES];
+			break;
+		/*case SIGN:
+			[self performSelectorOnMainThread:@selector(changeSignValue:) withObject:[NSMutableArray arrayWithObjects:*component,[Util getImageNameForSignValue:value],nil] waitUntilDone:YES];
+			break;
+		case INDICATOR:
+			 [self performSelectorOnMainThread:@selector(changeIndicatorValue:) withObject:[NSMutableArray arrayWithObjects:*component,[Util getImageNameForIndicatorType:componentSubtype withValue:value],[NSNumber numberWithInt:componentSubtype], nil] waitUntilDone:YES];
+			 break;*/
+		default:
+			break;
+	}
+}
+
+- (void) toggleVerbNounFlashStatus:(BOOL)flash{
+	NSLog(@"toggleVerbNounFlashStatus");
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	[self initSegmentDictionary];
+}
+
+
+
+@end
