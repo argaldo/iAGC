c8c2aa5d2bca56f547ca4c07f5c3431982f32167
argaldo <argaldo@gmail.com>
2011-02-21 18:30:47 +0100

    relocated IBAction

--- Classes/iPhone/Views/DSKYUIIphoneViewController.h
+++ Classes/iPhone/Views/DSKYUIIphoneViewController.h
@@ -45,27 +45,6 @@
 
 
 
-// action delegates for dsky keyboard click
-- (IBAction) pressedVerb: (id) sender;
-- (IBAction) pressedNoun: (id) sender;
-- (IBAction) pressedEnter: (id) sender;
-- (IBAction) pressedClr: (id) sender;
-- (IBAction) pressedPro: (id) sender;
-- (IBAction) pressedKeyRel: (id) sender;
-- (IBAction) pressedPlus: (id) sender;
-- (IBAction) pressedMinus: (id) sender;
-- (IBAction) pressedReset: (id) sender;
-- (IBAction) pressed1: (id) sender;
-- (IBAction) pressed2: (id) sender;
-- (IBAction) pressed3: (id) sender;
-- (IBAction) pressed4: (id) sender;
-- (IBAction) pressed5: (id) sender;
-- (IBAction) pressed6: (id) sender;
-- (IBAction) pressed7: (id) sender;
-- (IBAction) pressed8: (id) sender;
-- (IBAction) pressed9: (id) sender;
-- (IBAction) pressed0: (id) sender;
-
 - (IBAction) closeUplinkDataView: (id) sender;
 - (IBAction) showUplinkDataView: (id) sender;
 - (IBAction) monitorTimeUplink: (id) sender;
